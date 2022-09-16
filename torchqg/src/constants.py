64300fa29cb88a10993d9570367db705eadc2347
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2022-09-16 19:21:14 +0200

    Added a demo with a simpler QG model.

--- torchqg/src/constants.py
+++ torchqg/src/constants.py
@@ -1,7 +1,39 @@
 import math
+from dataclasses import dataclass
+
+@dataclass
+class Units:
+    
+    def unitless(x):
+        raise NotImplementedError()
+        
+    def real(x):
+        raise NotImplementedError()
+        
+        
+        
+class Time(Units):
+    conversion: float = 1.2e6
+    
+    def unitless(x):
+        return x * self.conversion
+    
+    def real(x):
+        return x / conversion
+    
+
 
 def t_unit():
     return 1.2e6
 
 def l_unit():
     return (504e4 / math.pi)
+
+
+def time_r2u(t):
+    return t / t_unit()
+
+def time_u2r(t):
+    return t * t_unit()
+
+# def space_2_unitless(
